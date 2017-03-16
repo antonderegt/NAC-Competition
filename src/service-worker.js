0fe82793b6d86f8b9258ad2b9e781d3602435662
Anton de Regt <antonderegt@gmail.com>
2017-03-16 19:42:30 +0100

    Update serviceworker cache name

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='v20'
-const CACHE_FILES=['./','./manifest.json','./index.html','./js/bundle.min.js','./images/icon-1x.png','./images/eggens.png','./images/ic_arrow_back_white_24dp_1x.png']
+const CACHE_NAME='v25'
+const CACHE_FILES=['./','./manifest.json','./index.html','./js/bundle.min.js','./images/eggens.png','./images/ic_arrow_back_white_24dp_1x.png']
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(cache=>{return cache.addAll(CACHE_FILES)}))})
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(cacheNames=>{return Promise.all(cacheNames.map(thisCacheName=>{if(thisCacheName!==CACHE_NAME){return caches.delete(thisCacheName)}}))}))})
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(response=>{if(response){return response}var fetchRequest=e.request.clone();return fetch(fetchRequest).then(response=>{if(!response){return response}
